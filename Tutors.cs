33a941c9e69c477146ccd2336f9c28fc77be2e11
danceofthejaberwocks <Suzanne.smith928@gmail.com>
2018-03-18 13:11:28 -0500

    Add files via upload

---
+++ Tutors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTutors.People
+{
+    class Tutors
+    {
+       
+        public string firstName;
+        public string lastName;
+        public int cost;
+        private int EarnedCredit(int payMe)
+        {
+            int a = 0;
+            a = payMe + a;
+            return a;
+        }
+        
+        public  string credit(int y)
+        {
+            y = EarnedCredit(y);
+                return $"{firstName} {lastName} has earned {y}";
+        }
+        
+    }
+}
