99014bc7f50d22cc6b54ca398a416201e33de360
danceofthejaberwocks <Suzanne.smith928@gmail.com>
2018-03-18 13:11:57 -0500

    Add files via upload

---
+++ Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheTutors.People;
+
+namespace TheTutors.People
+{
+    class Students
+    {
+        Tutors tutors = new Tutors();
+        public string firstName;
+        public string lastName;
+       
+
+    }
+}
+
